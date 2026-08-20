b232a7023e0e62b03d4ab097576526e02f2677b5
POOJA K K <poojakathirvelv@gmail.com>
2026-08-20 10:18:44 +0530

    Time: 0 ms (100%), Space: 43 MB (17.11%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String[] rows = {
+            "qwertyuiop",
+            "asdfghjkl",
+            "zxcvbnm"
+        };
+
+        List<String> ans = new ArrayList<>();
+
+        for (String word : words) {
+            String w = word.toLowerCase();
+
+            int row = -1;
+
+            for (int i = 0; i < 4; i++) {
+                if (rows[i].indexOf(w.charAt(0)) != -1) {
+                    row = i;
+                    break;
+                }
+            }
+
+            boolean valid = true;
+
+            for (char c : w.toCharArray()) {
+                if (rows[row].indexOf(c) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+                ans.add(word);
+        }
+
+        return ans.toArray(new String[0]);
+    }
+}
