d6e1469e9342084b950a74299231eaa3f4f72495
POOJA K K <poojakathirvelv@gmail.com>
2026-09-06 11:30:20 +0530

    Time: 1 ms (51.8%), Space: 43.7 MB (41.37%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+
+    private static final String[] map = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) return result;
+
+        backtrack(result, new StringBuilder(), digits, 0);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder path, String digits, int index) {
+        if (index == digits.length()) {
+            result.add(path.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for (char c : letters.toCharArray()) {
+            path.append(c);
+            backtrack(result, path, digits, index + 1);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+}
